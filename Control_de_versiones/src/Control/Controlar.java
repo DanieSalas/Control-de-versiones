ed0e7bfccb1d669f195dbaccf2cf488f5da39520
DanielSalas <149406765+DanieSalas@users.noreply.github.com>
2024-05-24 15:14:45 +0200

    Add files via upload

---
+++ Control_de_versiones/src/Control/Controlar.java
@@ -0,0 +1,7 @@
+package Control;
+
+public class Controlar {
+	public static void main(String[] args) {
+		System.out.println("Probando versiones para el mejor profe del mundo muldia chachi pistachi");
+}
+}
